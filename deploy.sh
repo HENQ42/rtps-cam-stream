a4aa8d7b90171e9c25930db94d16df9647ac31c7
henq42 <henriquebjr123@gmail.com>
2024-06-24 17:15:52 -0300

    update workflow

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Instalar dependências
+cd /home/henrique/web-henrique/rtps-cam-stream/client
+npm install
+
+cd /home/henrique/web-henrique/rtps-cam-stream/server
+npm install
+
+# Gerenciar processos com pm2
+pm2 kill
+pm2 delete rtsp-cam-stream-server || true
+pm2 delete rtsp-cam-stream-client || true
+
+# Iniciar server
+cd /home/henrique/web-henrique/rtps-cam-stream/server
+pm2 start server.js --name "rtsp-cam-stream-server"
+
+# Iniciar client
+cd /home/henrique/web-henrique/rtps-cam-stream/client
+pm2 start npm --name "rtsp-cam-stream-client" -- start
+
+# Salvar lista de processos
+pm2 save
+
+# Mostrar lista de processos
+pm2 list
--- start.sh
+++
@@ -1,5 +0,0 @@
-# cd /home/henrique/web-henrique/rtsp-cam-stream/client && npm install && npm start
-# cd /home/henrique/web-henrique/rtsp-cam-stream/server && npm install && node server.js
-
-cd server && npm install && node server.js &
-cd client && npm install && npm start
